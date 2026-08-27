c62ab69afd2516968ff4b36fd60e00026d069e86
Antonella Torr <bantonellatorres@gmail.com>
2026-08-27 14:54:05 -0300

    añado main

---
+++ xinu-avr-pse/main/tarea_controltyh.c
@@ -0,0 +1,51 @@
+#include <xinu.h>
+#include <stdint.h>
+#include <math.h>
+#include "twi.h"
+#include "sht31.h"
+#include "tarea_controltyh.h"
+#include "serial.h"
+
+extern float sht31_temperature;
+extern float sht31_humidity;
+
+//Se conecta scl a A5 y sda a A4
+
+int control_tyh(void){
+    int loopCnt = 0;
+    int enableHeater = 0;
+   
+    sleepms(5000);
+    
+    // Verificar si el sensor responde
+    if (!sht31_begin()) {
+        while(1) {
+            // Loop infinito de error
+            sleepms(1000);
+        }
+    }
+    serial_put_string("OK\n");
+    uint16_t estado = sht31_read_status();
+    serial_put_uint16_hex(estado);
+    serial_put_string("\n");
+
+    
+    float t,h;
+    while (1) {
+        if (sht31_read_both(&t, &h)) {
+
+            
+        } else {
+        }
+
+        sleepms(1000);
+
+        if (loopCnt >= 30) {
+            enableHeater = !enableHeater;
+            sht31_heater(enableHeater);
+            loopCnt = 0;
+        }
+
+        loopCnt++;
+    }
+}
---
+++ xinu-avr-pse/main/tarea_controltyh.h
@@ -0,0 +1,6 @@
+#ifndef _TYH_H
+#define _TYH_H
+
+int control_tyh(void);
+
+#endif /* _TYH_H */
